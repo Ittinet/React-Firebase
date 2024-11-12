92d4b7a4373b161473f36ccf732f7efe62276266
Saint <asdzxr2499@hotmail.com>
2024-11-12 21:46:18 +0700

    config Route

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,32 +1,55 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 
 import Shop from '../Shop'
 import Login from '../Login'
 import Register from '../../Register'
 
 
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Shop />
-    },
-    {
-        path: '/login',
-        element: <Login />
-    },
-    {
-        path: '/Register',
-        element: <Register />
-    }
-])
-
 
 const AppRoutes = () => {
     return (
-        <>
-            <RouterProvider router={router} />
-        </>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Shop />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+            </Routes>
+        </Router>
     )
 }
 
 export default AppRoutes
+
+
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+// import Shop from '../Shop'
+// import Login from '../Login'
+// import Register from '../../Register'
+
+
+// const router = createBrowserRouter([
+//     {
+//         path: '/',
+//         element: <Shop />
+//     },
+//     {
+//         path: '/login',
+//         element: <Login />
+//     },
+//     {
+//         path: '/Register',
+//         element: <Register />
+//     }
+// ])
+
+
+// const AppRoutes = () => {
+//     return (
+//         <>
+//             <RouterProvider router={router} />
+//         </>
+//     )
+// }
+
+// export default AppRoutes
